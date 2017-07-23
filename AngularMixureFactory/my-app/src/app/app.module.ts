809ae3411326335ab2a7ea57599be09a981bef56
Rikka Ramakrishna <Rikka Ramakrishna>
2017-07-23 17:28:40 +0530

    Committed the changes for routing links in page.

--- AngularMixureFactory/my-app/src/app/app.module.ts
+++ AngularMixureFactory/my-app/src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginUserComponent } from './login-user/login-user.component';
 
+import {RouterModule,Routes} from '@angular/router';
+
+
+const appRoutes: Routes= [
+  {path:'registration',component:RegistrationComponent},
+  {path: 'login',component:LoginUserComponent}
+
+];
+
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent,
     RegistrationComponent,
     LoginUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
