6774b791cfdc3d09ec86d256950372cc6d7a65e2
BijayaKBhatta <kishor.bijaya@gmail.com>
2017-07-29 09:20:27 -0400

    added the changes related to css

--- AngularMixureFactory/my-app/src/app/app.component.ts
+++ AngularMixureFactory/my-app/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Best Mixute Production Factory';
-  
+  title = 'Best Mixute Production Factory';  
 }
